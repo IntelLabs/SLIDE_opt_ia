5e18cacb6cf5495e44b63f96bd5b8a10b023f95a
Yong Wu <yong.wu@intel.com>
2020-04-25 02:29:52 +0800

    Flush denormal to zero
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- SLIDE/main.cpp
+++ SLIDE/main.cpp
@@ -595,6 +595,11 @@ int main(int argc, char* argv[]) {
   //***********************************
   parseconfig(argv[1]);
 
+#if OPT_IA
+  _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+  _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+#endif
+
   if (Bfloat16Opt)
     return Execute<bfloat16>();
   else
